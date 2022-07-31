8498c6a5a44927e23e56b2d4edf475afc9d4952a
Mateen <zahidx297@yahoo.com>
2022-07-31 02:35:08 -0400

    add operate function

--- javascript.js
+++ javascript.js
@@ -3,3 +3,16 @@ function subtract(a, b) {return a - b;}
 function multiply(a, b) {return a * b;}
 function divide(a, b) {return a / b;}
 
+function operate(op, a, b) {
+    switch (op) {
+        case 'add':
+            return add(a, b);
+        case 'sub':
+            return subtract(a, b);
+        case 'mul':
+            return multiply(a, b);
+        case 'div':
+            return divide(a, b); 
+    }
+}
+
