55a2dbe053b846afffabf5e66a8a64e24cb2d81c
Mateen <zahidx297@yahoo.com>
2022-07-31 04:50:57 -0400

    add buffer object to hold sets of operand/operators

--- javascript.js
+++ javascript.js
@@ -17,31 +17,55 @@ function operate(op, a, b) {
 }
 
 
+function updateScreen() {
+    let text = '';
+    buffer.items.forEach(item => {
+        text += item.value;
+    });
+    screen.innerText = (text.length==0) ? '0'
+            : text.slice(-MAX_CHAR);    
+}
+
+const buffer = {
+    items: [],
+    getType() {
+        let length = this.items.length;
+        if (length == 0)
+            return 'empty';
+        else
+            return this.items[length-1].type;
+    },
+    addOperand(value) {
+        this.items.push({type: 'operand', value: value});
+    },
+    appendOperand(value) {
+        this.items[this.items.length-1].value += value;
+    }
+}
+
 const screen = document.querySelector('.screen');
 const MAX_CHAR = 16;
 let fullScreenText = '';
 updateScreen();
-
-function updateScreen() {
-    screen.innerText = (fullScreenText.length==0) ? '0'
-            : fullScreenText.slice(-MAX_CHAR);    
-}
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', update);
 });
 
+
 function update() {
     let value = this.innerText;
     let type = this.dataset.type;
-    switch (value) {
-        case 'clr':
+
+    switch (type) {
+        case 'num':
+            if (buffer.getType() == 'empty')
+                buffer.addOperand(value);
+            else if (buffer.getType()=== 'operand')
+                buffer.appendOperand(value);
             break;
-        case '=':
-            break;
-        default:
-            fullScreenText += value;
-            updateScreen();
     }
+    console.log(buffer.items);
+    updateScreen();
 }
